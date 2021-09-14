2452d0b942980cd4fe7ee9697364d673b67a7339
Pavel Korol <p_korol@wargaming.net>
2021-09-14 21:45:32 +0300

    fx

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,10 @@ var swiper = new Swiper(".slider", {
         }
     },
 });
+
+// texts
+const randomTextElements = document.querySelectorAll('.js-random-text');
+const randomIndex = Math.floor(Math.random(0, 2) * 3);
+
+randomTextElements.forEach(el => el.style.display = 'none');
+document.querySelectorAll(`.js-random-text:nth-child(${randomIndex + 1})`).forEach(el => el.style.display = 'block');
