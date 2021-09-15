bddd8c072a901ac3d30cada4a9120766dc6df4cf
Pavel Korol <p_korol@wargaming.net>
2021-09-15 22:07:54 +0300

    form

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,36 @@ const randomIndex = Math.floor(Math.random(0, 2) * 3);
 
 randomTextElements.forEach(el => el.style.display = 'none');
 document.querySelectorAll(`.js-random-text:nth-child(${randomIndex + 1})`).forEach(el => el.style.display = 'block');
+
+
+// form 
+const submitButton = document.querySelector('input[type="submit"]');
+const form = document.querySelector('form');
+submitButton.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    let formData = new FormData();
+    document.querySelectorAll('input[name]').forEach(el => {
+        formData.append(el.name, el.value);
+    })
+
+    fetch(form.action,
+        {
+            body: formData,
+            method: "post"
+        });
+
+    form.classList.add('done');
+})
+
+
+const copyButton = document.querySelector('.js-copy-button');
+copyButton.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    navigator.clipboard.writeText(location).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+})
